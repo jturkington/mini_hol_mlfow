6226d0016bed9f562b228031eef4fd0e82fdf5bd
jturkington <95910004+jturkington@users.noreply.github.com>
2025-08-12 10:26:06 +0100

    Update 0 - bootstrap.py

--- 0 - bootstrap.py
+++ 0 - bootstrap.py
@@ -1,4 +1,5 @@
 # -------------------------------
-# 1. Donwload Required Python Libaries
+# 1. Download Required Python Packages
 # -------------------------------
 !pip3 install -r requirements.txt
+print("PYTHON PACKAGES ARE INSTALLED")
